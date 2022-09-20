f0088e81d53ae0ad328fa92730bda363e5593f55
aimanrzq <muhamad.aimanrzq@gmail.com>
2022-09-20 18:31:08 +0800

    remove and added relevant file

---
+++ src/components/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+
+function App() {
+
+    const [listItem, setList] = useState([]);
+
+    function addItem(inputText){
+        setList((prevValue) => {
+            return [
+                ...prevValue, inputText
+            ]
+        });
+        
+    };
+
+    function deleteItem (id){
+        setList((prevValue) => {
+            return prevValue.filter((item, index) => {
+                return index !== id
+            })
+        })
+    }
+
+    return (
+        <div>
+            < Header />
+            <CreateArea onAdd={addItem}/>
+            {listItem.map((todoItem, index) => {
+                return (
+                    < Note
+                    id={index} 
+                    key={index}
+                    title={todoItem.title}
+                    content={todoItem.content}
+                    onDelete={deleteItem}
+                />
+                )
+            })}
+            < Footer />
+        </div>
+
+    )
+}
+
+export default App;
